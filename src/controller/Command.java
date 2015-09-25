840df2a74c3accd069c0e4afdface08b007af892
Yoav Stezki <yoavStezki@gmail.com>
2015-09-25 15:15:21 +0300

    delete files

--- src/controller/Command.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import java.util.List;
-
-public interface Command
-{
-    void doCommand(List<String> args);
-}
--- src/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import java.util.HashMap;
-
-public interface Controller
-{
-   HashMap<String, Command> createCommandNameToCommand();
-}
